b40da83b9a56dc6f7e08cbf75607ae22f4e5adc4
Datisto <nickyettikiller@gmail.com>
2025-11-29 19:03:22 +0200

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Skull, Users, Shield, BookOpen, Play } from 'lucide-react';
+import { Skull, Users, Shield, BookOpen, Play, Map } from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext';
 
 export default function LandingPage() {
@@ -147,7 +147,7 @@ export default function LandingPage() {
               </div>
 
 <div className="bg-gray-800 bg-opacity-60 p-6 rounded-lg border border-gray-700 hover:border-red-500 transition">
-                <BookOpen className="w-12 h-12 text-red-500 mb-4" />
+                <Map className="w-12 h-12 text-red-500 mb-4" />
                 <h4 className="text-xl font-semibold mb-3">Мала ЗОНА</h4>
                 <p className="text-gray-400">
                   У нас своя мала зона — місце, куди потрапляють усі хто хоче спробувати складне виживання в сталкер рп без заморочок.<br /><br />
