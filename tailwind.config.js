528517c56090f663da74326c6d67099c427f7547
Datisto <nickyettikiller@gmail.com>
2025-11-30 00:22:15 +0200

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'stalker': ['"AmazStalker"', 'cursive'],
+        'stalker': ['AmazStalker', 'cursive'],
       },
     },
   },
