52d243b7eaf5f61c7af31497cc34921cb6fa3066
nickolanico900-a11y <nickolanico900@gmail.com>
2025-12-12 23:12:06 +0200

    Updated characters.js

--- server/src/routes/characters.js
+++ server/src/routes/characters.js
@@ -85,10 +85,10 @@ router.post('/', authenticateUser, async (req, res) => {
         appearance, psychological_portrait, character_traits, skills, inventory
       ) VALUES (UUID(), ?, ?, 'draft', ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
       [
-        req.user.id, req.user.steam_id, name, surname, patronymic, nickname, age, gender,
+        req.user.id || null, req.user.steam_id, name, surname, patronymic, nickname, age, gender,
         face_model, origin_country, citizenship, faction, biography,
         appearance, psychological_portrait,
-        JSON.stringify(character_traits), JSON.stringify(skills), JSON.stringify(inventory)
+        JSON.stringify(character_traits || []), JSON.stringify(skills || []), JSON.stringify(inventory || [])
       ]
     );
 
